33ec8ad6ceb060c27df124ebeaf9c5502bc43817
you@example.com <you@example.com>
2022-03-23 14:39:55 +0530

    Auto-generated commit [ci skip]

--- projects/twilio-video/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of twilio-video
- */
-
-export * from './lib/twilio-video.service';
-export * from './lib/twilio-video.component';
-export * from './lib/twilio-video.module';
